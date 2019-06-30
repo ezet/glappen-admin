0dcea2210c98dbdd02a797831e16c60f85b730c9
Andreas Ore Larssen <andreasol@me.com>
2019-06-30 19:56:56 +0200

    WIP: wardrobe ui

--- lib/ui/wardrobe.dart
+++ lib/ui/wardrobe.dart
@@ -34,25 +34,55 @@ class CoatHangerList extends StatelessWidget {
         child: CircularProgressIndicator(),
       );
     }
-    return ListView.separated(
-      padding: EdgeInsets.all(10),
-      itemCount: hangers.length,
-      separatorBuilder: (BuildContext context, int index) => Divider(
-            height: 30,
-            color: Colors.transparent,
+    return Row(
+      children: <Widget>[
+        Expanded(
+          flex: 3,
+          child: ListView.separated(
+            padding: EdgeInsets.all(10),
+            itemCount: hangers.length,
+            separatorBuilder: (BuildContext context, int index) => Divider(
+                  height: 20,
+                  color: Colors.transparent,
+                ),
+            itemBuilder: (context, i) {
+              if (hangers[i].state == HangerState.CHECKING_IN) {
+                return _buildCheckInItem(context, hangers, i, api);
+              } else {
+                return _buildCheckOutItem(context, hangers, i, api);
+              }
+            },
           ),
-      itemBuilder: (context, i) {
-        if (hangers[i].state == HangerState.CHECKING_IN) {
-          return buildCheckInItem(context, hangers, i, api);
-        } else {
-          // TODO: implement check-out layout
-          return buildCheckInItem(context, hangers, i, api);
-        }
-      },
+        ),
+        Expanded(
+          flex: 2,
+          child: Container(
+            color: Color.fromRGBO(0, 0, 0, 0.3),
+          ),
+        )
+      ],
     );
   }
 
-  Container buildCheckInItem(
+  Container _buildCheckInItem(
+      BuildContext context, List<CoatHanger> hangers, int i, GladminApi api) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(47, 51, 54, 1),
+        borderRadius: BorderRadius.all(
+          Radius.circular(15.0),
+        ),
+        border: Border.all(
+          color: Colors.amber,
+          width: 1.0,
+          style: BorderStyle.solid,
+        ),
+      ),
+      height: 200,
+    );
+  }
+
+  Container _buildCheckOutItem(
       BuildContext context, List<CoatHanger> hangers, int i, GladminApi api) {
     return Container(
       height: 110,
@@ -61,102 +91,68 @@ class CoatHangerList extends StatelessWidget {
         borderRadius: BorderRadius.all(
           Radius.circular(15.0),
         ),
-        border:
-            Border.all(color: Color.fromRGBO(66, 74, 82, 1), width: 1.0, style: BorderStyle.solid),
-        boxShadow: [
+        border: Border.all(
+            color: Color.fromRGBO(66, 74, 82, 1),
+            width: 1.0,
+            style: BorderStyle.solid),
+        /*boxShadow: [
           BoxShadow(
             color: Color.fromRGBO(0, 147, 255, .10),
             blurRadius: 6,
           ),
-        ],
+        ],*/
       ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           Expanded(
-            flex: 2,
+            flex: 5,
             child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(14),
-                  bottomLeft: Radius.circular(14),
-                ),
-                color: Color.fromRGBO(17, 21, 24, 1),
-              ),
-              child: Material(
-                color: Colors.transparent,
-                child: InkWell(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(14),
-                    bottomLeft: Radius.circular(14),
-                  ),
-                  splashColor: Color.fromRGBO(255, 75, 75, 1),
-                  onTap: () async {
-                    var result = await api.rejectUpdate(hangers[i]);
-                  },
-                  child: Container(
-                    child: Center(
-                      child: Icon(
-                        Icons.cancel,
-                        color: Color.fromRGBO(255, 75, 75, 1),
-                        size: 33,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Container(
+              padding: EdgeInsets.only(left: 20),
               decoration: BoxDecoration(
                 border: Border(
-                  left: BorderSide(
-                    width: 1,
-                    color: Color.fromRGBO(66, 74, 82, 1),
-                  ),
                   right: BorderSide(
                     width: 1,
                     color: Color.fromRGBO(66, 74, 82, 1),
                   ),
                 ),
               ),
-              child: Material(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Text(
-                      hangers[i].state == HangerState.CHECKING_OUT ? "OUT" : "IN",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30,
-                      ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  /*Text(
+                    hangers[i].state == HangerState.CHECKING_OUT ? "OUT" : "IN",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
                     ),
-                    Text(
-                      hangers[i].id.toString(),
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30,
-                      ),
+                  ),*/
+                  Text(
+                    hangers[i].id.toString(),
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
                     ),
-                    StreamBuilder<User>(
-                        stream: hangers[i].getUser(),
-                        builder: (context, userSnapshot) => InkWell(
-                              onTap: () => Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => Profile(userSnapshot.data.docId))),
-                              child: Text(
-                                userSnapshot.data?.name ?? "",
-                                maxLines: 1,
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w400,
-                                ),
+                  ),
+                  StreamBuilder<User>(
+                      stream: hangers[i].getUser(),
+                      builder: (context, userSnapshot) => InkWell(
+                            onTap: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        Profile(userSnapshot.data.docId))),
+                            child: Text(
+                              userSnapshot.data?.name ?? "",
+                              maxLines: 1,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 20,
                               ),
-                            ))
-                  ],
-                ),
+                            ),
+                          ))
+                ],
               ),
             ),
           ),
